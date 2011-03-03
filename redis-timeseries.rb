460a8e025edb1042fbad1f5877569a15e9d854a9
antirez <antirez@gmail.com>
2011-03-03 18:41:59 +0100

    binary search fixed for all the edge cases

--- redis-timeseries.rb
+++ redis-timeseries.rb
@@ -54,13 +54,15 @@ class RedisTimeSeries
     end
 
     def seek(time)
+        best_start = nil
+        best_time = nil
         rangelen = 64
         key = getkey(time.to_i)
         len = @redis.strlen(key)
         return 0 if len == 0
         min = 0
         max = len-1
-        while min < max
+        while true
             p = min+((max-min)/2)
             puts "Min: #{min} Max: #{max} P: #{p}"
             # Seek the first complete record starting from position 'p'.
@@ -68,36 +70,48 @@ class RedisTimeSeries
             # the range if needed as we don't know how big the record is.
             while true
                 range_end = p+rangelen-1
-                range_end = max if range_end > max
+                range_end = len if range_end > len
                 r = @redis.getrange(key,p,range_end)
-                sep = r.index("\x00")
+                # puts "GETRANGE #{p} #{range_end}"
+                if p == 0
+                    sep = -1
+                else
+                    sep = r.index("\x00")
+                end
                 sep2 = r.index("\x00",sep+1) if sep
                 if sep and sep2
                     record = r[((sep+1)...sep2)]
                     record_start = p+sep+1
                     record_end = p+sep2-1
+                    dr = decode_record(record)
+
+                    # Take track of the best sample, that is the sample
+                    # that is greater than our sample, but with the smallest
+                    # increment.
+                    if dr[:time] >= time and (!best_time or best_time>dr[:time])
+                        best_start = record_start
+                        best_time = dr[:time]
+                        # puts "NEW BEST: #{best_time}"
+                    end
+                    # puts "Max-Min #{max-min} RS #{record_start}"
+                    return best_start if max-min == 1
                     break
                 end
-                if range_end >= max
-                    record = nil
-                    break
-                end
+                # Already at the end of the string but still no luck?
+                return len+1 if range_end = len
                 # We need to enlrange the range, it is interesting to note
                 # that we take the enlarged value: likely other time series
                 # will be the same size on average.
                 rangelen *= 2
             end
-            return max+1 if !record
-            dr = decode_record(record)
-            puts dr.inspect
+            # puts dr.inspect
             return record_start if dr[:time] == time
             if dr[:time] > time
-                max = record_start-1
+                max = p
             else
-                min = record_start-1
+                min = p
             end
         end
-        return min+1
     end
 end
 
@@ -105,6 +119,7 @@ ts = RedisTimeSeries.new("test",3600*24,Redis.new)
 puts ts.seek(1299142304.70)
 puts ts.seek(1299142311.3)
 puts ts.seek(1299142301.2)
+puts ts.seek(1299142401.2)
 =begin
 (0..100).each{|i|
     ts.add(i.to_s)
